6ad3d3099ce167e1cd040bfdfcdd8f620cd5477f
Pierre Braem <pierrebraem@orange.fr>
2020-05-18 16:50:43 +0200

    Création du menu

---
+++ mnuInscription.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Gestion_de_musique
+{
+    partial class mnuInscription
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnConsulter = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(124, 37);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(179, 133);
+            this.btnAjouter.TabIndex = 0;
+            this.btnAjouter.Text = "Ajouter une inscription";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            // 
+            // btnConsulter
+            // 
+            this.btnConsulter.Location = new System.Drawing.Point(496, 37);
+            this.btnConsulter.Name = "btnConsulter";
+            this.btnConsulter.Size = new System.Drawing.Size(193, 133);
+            this.btnConsulter.TabIndex = 1;
+            this.btnConsulter.Text = "Consulter";
+            this.btnConsulter.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(124, 231);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(179, 118);
+            this.btnSupprimer.TabIndex = 2;
+            this.btnSupprimer.Text = "Supprimer une inscription";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(496, 231);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(193, 118);
+            this.btnModifier.TabIndex = 3;
+            this.btnModifier.Text = "Modifier une inscription";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            // 
+            // mnuInscription
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnConsulter);
+            this.Controls.Add(this.btnAjouter);
+            this.Name = "mnuInscription";
+            this.Text = "mnuInscription";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnConsulter;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnModifier;
+    }
+}
