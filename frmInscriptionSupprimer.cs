72134433edce2cdea4caa2169f7559dc5e112ee2
Pierre Braem <pierrebraem@orange.fr>
2020-05-18 16:46:10 +0200

    Création du formulaire Supprimer

---
+++ frmInscriptionSupprimer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_de_musique
+{
+    public partial class frmInscriptionSupprimer : Form
+    {
+        public frmInscriptionSupprimer()
+        {
+            InitializeComponent();
+        }
+    }
+}
